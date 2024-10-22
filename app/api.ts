cbec4caff2d372e98887960074a097188caa355d
Ken Low <kenlck1990@gmail.com>
2024-10-22 13:54:33 +0800

    Setup tanstack start

---
+++ app/api.ts
@@ -0,0 +1,4 @@
+// app/api.ts
+import { createStartAPIHandler, defaultAPIFileRouteHandler } from "@tanstack/start/api";
+
+export default createStartAPIHandler(defaultAPIFileRouteHandler);
---
+++ app/routes/__root.tsx
@@ -0,0 +1,49 @@
+// app/routes/__root.tsx
+import { createRootRoute } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
+import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
+import * as React from "react";
+import tailwindCss from "../tailwind.css?url";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  meta: () => [
+    {
+      charSet: "utf-8",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    {
+      title: "TanStack Start Starter",
+    },
+  ],
+  links: () => [{ rel: "stylesheet", href: tailwindCss }],
+  component: RootComponent,
+  notFoundComponent: () => <h1>Not Found</h1>,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <Html>
+      <Head>
+        <Meta />
+      </Head>
+      <Body>
+        {children}
+        <ScrollRestoration />
+        <TanStackRouterDevtools position="bottom-right" />
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
